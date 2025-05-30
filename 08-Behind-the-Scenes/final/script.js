3a9b583ef2835fa526af95ee028b70c835c4c409
Mihiranga <nmih0313@sysco.com>
2025-05-30 13:50:25 +0530

    Add age calculation and millennial check functionality

--- 08-Behind-the-Scenes/final/script.js
+++ 08-Behind-the-Scenes/final/script.js
@@ -221,3 +221,38 @@ jessicaClone.familiy.push('John');
 console.log('Original:', jessica);
 console.log('Clone:', jessicaClone);
 */
+
+
+function calcAge(birthYear){
+  const age = 2037 - birthYear;
+
+
+  function printAge(){
+    let output  = `${firstName},You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if(birthYear >= 1981 && birthYear <= 1996){
+      var milenial = true;
+
+      //Creatiing New variable with same name as outer scope's variable
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b){
+        return a + b;
+      }
+
+      const output = 'NEW OUTPUT!';
+    }
+    // console.log(str);
+    console.log(milenial);
+    // console.log(add(2,3));
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// printAge();
