4edd01fadd8d7c66e76e052eaf52e723f4fa15b7
Nethum99 <nethummihiranga20182018@gmail.com>
2025-05-31 23:45:32 +0530

    Practice windows keywords

--- 08-Behind-the-Scenes/final/script.js
+++ 08-Behind-the-Scenes/final/script.js
@@ -223,36 +223,45 @@ console.log('Clone:', jessicaClone);
 */
 
 
-function calcAge(birthYear){
-  const age = 2037 - birthYear;
+// function calcAge(birthYear){
+//   const age = 2037 - birthYear;
 
 
-  function printAge(){
-    let output  = `${firstName},You are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge(){
+//     let output  = `${firstName},You are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if(birthYear >= 1981 && birthYear <= 1996){
-      var milenial = true;
+//     if(birthYear >= 1981 && birthYear <= 1996){
+//       var milenial = true;
 
-      //Creatiing New variable with same name as outer scope's variable
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//       //Creatiing New variable with same name as outer scope's variable
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b){
-        return a + b;
-      }
+//       function add(a, b){
+//         return a + b;
+//       }
 
-      const output = 'NEW OUTPUT!';
-    }
-    // console.log(str);
-    console.log(milenial);
-    // console.log(add(2,3));
-    console.log(output);
-  }
-  printAge();
-  return age;
-}
+//       const output = 'NEW OUTPUT!';
+//     }
+//     // console.log(str);
+//     console.log(milenial);
+//     // console.log(add(2,3));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
 
-const firstName = 'Jonas';
-calcAge(1991);
-// printAge();
+// const firstName = 'Jonas';
+// calcAge(1991);
+// // printAge();
+
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
